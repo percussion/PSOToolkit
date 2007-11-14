56823db1d7f53a8a7c93981a30be9b9d1dce7bac
David Benua <david_benua@percussion.com>
2007-11-14 15:52:09 +0000

    Experiment in Nav Tools.  Don't use for anything

--- src/com/percussion/pso/jexl/PSONavTools.java
+++ src/com/percussion/pso/jexl/PSONavTools.java
@@ -9,17 +9,54 @@
  */
 package com.percussion.pso.jexl;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
 import javax.jcr.ItemNotFoundException;
 import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import com.percussion.cms.PSCmsException;
+import com.percussion.design.objectstore.PSLocator;
 import com.percussion.extension.IPSJexlExpression;
 import com.percussion.extension.IPSJexlMethod;
 import com.percussion.extension.IPSJexlParam;
 import com.percussion.extension.PSExtensionProcessingException;
 import com.percussion.extension.PSJexlUtilBase;
+import com.percussion.services.assembly.IPSAssemblyErrors;
+import com.percussion.services.assembly.IPSAssemblyItem;
+import com.percussion.services.assembly.IPSAssemblyResult;
+import com.percussion.services.assembly.IPSAssemblyService;
+import com.percussion.services.assembly.IPSSlotContentFinder;
+import com.percussion.services.assembly.IPSTemplateSlot;
+import com.percussion.services.assembly.PSAssemblyException;
+import com.percussion.services.assembly.PSAssemblyServiceLocator;
+import com.percussion.services.assembly.data.PSAssemblyWorkItem;
+import com.percussion.services.assembly.impl.nav.PSNavHelper;
+import com.percussion.services.assembly.jexl.PSAssemblerUtils;
+import com.percussion.services.contentmgr.IPSNode;
+import com.percussion.services.filter.PSFilterException;
+import com.percussion.services.general.IPSRhythmyxInfo;
+import com.percussion.services.general.PSRhythmyxInfoLocator;
+import com.percussion.services.guidmgr.IPSGuidManager;
+import com.percussion.services.guidmgr.PSGuidManagerLocator;
+import com.percussion.services.guidmgr.data.PSLegacyGuid;
+import com.percussion.services.sitemgr.PSSiteHelper;
+import com.percussion.services.utils.jexl.PSServiceJexlEvaluatorBase;
+import com.percussion.util.IPSHtmlParameters;
+import com.percussion.utils.exceptions.PSBaseException;
+import com.percussion.utils.jexl.PSJexlEvaluator;
 
 /**
  * Binding functions usefule for Navigation.
@@ -36,6 +73,11 @@ public class PSONavTools extends PSJexlUtilBase implements IPSJexlExpression
     */
    private static final Log log = LogFactory.getLog(PSONavTools.class);   
    
+   private static IPSGuidManager gmgr = null; 
+   
+   private static IPSRhythmyxInfo rxinfo = null; 
+   
+   private static IPSAssemblyService asm = null; 
    /**
     * 
     */
@@ -118,4 +160,129 @@ public class PSONavTools extends PSJexlUtilBase implements IPSJexlExpression
       
       return ancestors;
    }
+   
+   @IPSJexlMethod(description="Gets the Nav Node for a Page item",
+          params={@IPSJexlParam(name="assemblyItem", description="Assembly Item to find Nav Node")})
+   public Map<String,Object> getNavNode(IPSAssemblyItem assemblyItem) 
+      throws PSCmsException, PSFilterException, RepositoryException
+   {
+      initServices(); 
+      PSAssemblyWorkItem work = (PSAssemblyWorkItem)assemblyItem;
+      PSJexlEvaluator eval = new PSJexlEvaluator(assemblyItem.getBindings());
+      PSLocator home = gmgr.makeLocator(work.getId()); 
+      log.debug("home is " + home.getId());
+      PSNavHelper helper =  work.getNavHelper();
+      
+      IPSNode navon = (IPSNode) helper.findNavNode(work);
+      if(navon == null)
+      {
+         log.warn("cannot find navon for item");
+         return new HashMap<String, Object>(); 
+      }
+      PSLocator navLoc = gmgr.makeLocator(navon.getGuid());
+      log.debug("nav is " + navLoc.getId());
+     
+      helper.setupNavValues(work, eval, navon);
+      Map<String,Object> nav = (Map)work.getBindings().get("$nav");  
+      IPSNode root = (IPSNode) nav.get("root"); 
+      PSLocator rootLoc = gmgr.makeLocator(root.getGuid());
+      log.debug("root is " + rootLoc.getId()); 
+      IPSNode self = (IPSNode) nav.get("self");
+      PSLocator selfLoc = gmgr.makeLocator(self.getGuid());
+      log.debug("self is " + rootLoc.getId()); 
+      
+      return nav;
+   }
+
+   @IPSJexlMethod(description="Gets the Nav Node for a Page item",
+         params={@IPSJexlParam(name="assemblyItem", description="Assembly Item to find Nav Node")})
+   public Map<String, Object> getNav(IPSAssemblyItem assemblyItem) throws Throwable
+   {
+      IPSAssemblyResult navon = getAssembledNavon(assemblyItem);
+      log.debug("Navon id is " + navon.getId()); 
+      Map<String,Object> bindings = navon.getBindings(); 
+      Map<String,Object> nav = new HashMap<String, Object>(); 
+      nav.putAll((Map<String, Object>) bindings.get("$nav"));
+      return nav; 
+   }
+   
+   
+   
+   private IPSAssemblyResult getAssembledNavon(IPSAssemblyItem sysItem) throws Throwable 
+   {
+       initServices();
+       IPSAssemblyItem item = (IPSAssemblyItem) sysItem.clone(); 
+       String emsg; 
+       Properties navProps = getNavProperties(); 
+       String navSlotName = StringUtils.substringBefore(navProps.getProperty(NAVON_SLOTNAMES), ",");
+       log.debug("Navon slot name " + navSlotName); 
+       String navTemplateName = navProps.getProperty(NAVON_VARIANT_INFO);
+       log.debug("Navon template name " + navTemplateName); 
+       IPSTemplateSlot navSlot = asm.findSlotByName(navSlotName); 
+       PSAssemblerUtils asmHelper = new PSAssemblerUtils();
+       Map<String,Object> slotParms = new HashMap<String, Object>(); 
+       slotParms.put("template", navTemplateName);
+       
+       //Map<String, String> variables = item.getVariables();
+       Map<String,String[]> sysParams = item.getParameters();
+       
+       //String siteid = sysParams.get(IPSHtmlParameters.SYS_SITEID)[0]; 
+       //String contextid = sysParams.get(IPSHtmlParameters.SYS_CONTEXT)[0]; 
+      // PSSiteHelper.setupSiteInfo(eval, siteid, contextid);
+       Map<String, Object> combinedParms = asmHelper.combine(sysParams, slotParms); 
+       List<IPSAssemblyResult> results = asmHelper.assemble(item, navSlot, combinedParms); 
+       if(results.size() == 0)
+       {
+          emsg = "unable to assemble navon in slot " + navSlotName + " for item " + item.getId(); 
+          log.error(emsg); 
+          throw new RuntimeException(emsg); 
+       }
+       return results.get(0); 
+   }
+   
+   private static void initServices()
+   {
+      if(gmgr == null)
+      {
+         gmgr = PSGuidManagerLocator.getGuidMgr(); 
+         rxinfo = PSRhythmyxInfoLocator.getRhythmyxInfo(); 
+         asm = PSAssemblyServiceLocator.getAssemblyService(); 
+         
+         navPropertiesFile = (String)rxinfo.getProperty(IPSRhythmyxInfo.Key.ROOT_DIRECTORY)
+         + File.separator + "rxconfig/Server/Navigation.properties";
+
+      }
+   }
+   
+   protected synchronized Properties getNavProperties()
+   {
+      initServices(); 
+      File propsFile = new File(navPropertiesFile);
+      long lastmodified = propsFile.lastModified();
+      if(navProperties == null || lastmodified != npLastModified )
+      {
+         navProperties = new Properties();
+         try
+         {
+            FileInputStream fis = new FileInputStream(propsFile);
+            navProperties.load(fis);
+         } catch (Exception ex)
+         {
+            log.error("Exception loading navProperties " + ex,ex);
+            throw new RuntimeException("unable to load nav properties"); 
+         } 
+      }
+      
+      return navProperties;
+   }
+   
+   private long npLastModified = 0L; 
+   
+   private Properties navProperties = null;
+   
+   private static String navPropertiesFile = null; 
+   
+   public static final String NAVON_SLOTNAMES = "navon.slotnames"; 
+   
+   public static final String NAVON_VARIANT_INFO = "navon.variant.info"; 
 }
