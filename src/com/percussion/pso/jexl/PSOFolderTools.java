d853e4041d1ff57cdc4c4bde6f37a9f5303e03a4
Adam Gent <adam_gent@percussion.com>
2007-11-19 22:03:42 +0000

    Fixed getParentFolderPath so that it works with out sys_folderid being passed.

--- src/com/percussion/pso/jexl/PSOFolderTools.java
+++ src/com/percussion/pso/jexl/PSOFolderTools.java
@@ -11,11 +11,14 @@ package com.percussion.pso.jexl;
 import java.io.File;
 import java.util.List;
 
+import javax.jcr.RepositoryException;
+
 import static java.util.Arrays.asList;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.percussion.cms.PSCmsException;
 import com.percussion.cms.objectstore.PSFolder;
 import com.percussion.design.objectstore.PSLocator;
 import com.percussion.extension.IPSExtensionDef;
@@ -26,6 +29,10 @@ import com.percussion.extension.PSExtensionException;
 import com.percussion.extension.PSExtensionProcessingException;
 import com.percussion.extension.PSJexlUtilBase;
 import com.percussion.services.assembly.IPSAssemblyItem;
+import com.percussion.services.assembly.data.PSAssemblyWorkItem;
+import com.percussion.services.assembly.impl.nav.PSNavHelper;
+import com.percussion.services.contentmgr.IPSNode;
+import com.percussion.services.filter.PSFilterException;
 import com.percussion.services.guidmgr.IPSGuidManager;
 import com.percussion.services.guidmgr.PSGuidManagerLocator;
 import com.percussion.utils.guid.IPSGuid;
@@ -105,15 +112,69 @@ public class PSOFolderTools extends PSJexlUtilBase implements IPSJexlExpression
            params={@IPSJexlParam(name="assemblyItem", description="$sys.assemblyItem")},
            returns="the path of the folder that contains this item"
    )
-   public String getParentFolderPath(IPSAssemblyItem assemblyItem) throws PSErrorResultsException { 
-       int id = assemblyItem.getFolderId();
-       if (id < 0) return null;
-       IPSGuid folderGuid = guidManager.makeGuid(new PSLocator(id, -1));
-       List<PSFolder> folders = contentWs.loadFolders(asList(folderGuid));
-       if (folders.size() < 1) return null;
-       return folders.get(0).getFolderPath();
-       
-   }
+   public String getParentFolderPath(IPSAssemblyItem assemblyItem)
+            throws PSErrorResultsException, PSExtensionProcessingException, PSErrorException {
+        int id = assemblyItem.getFolderId();
+        String path = null;
+        /*
+         * If there is no folder id associated with the assembly item
+         * (ie sys_folderid was not passed as a parameter) then we are going
+         * to have to lookup the folder using the same process that managed nav
+         * does.
+         * Unfortuanatly this process is tightly coupled to Nav so we have
+         * to instantiate the PSNavHelper class instead of using a service.
+         * TODO Dave should look over this.
+         */
+        if (id <= 0) {
+            log.debug("Assembly Item does not have a folder id.");
+            if (assemblyItem instanceof PSAssemblyWorkItem) {
+                PSAssemblyWorkItem awi = (PSAssemblyWorkItem) assemblyItem;
+                PSNavHelper helper = awi.getNavHelper();
+                if (helper != null) {
+                    log.debug("Using NavHelper to find folder id.");
+                    String errMesg = "Tried to use NavHelper to get parent folder path but failed!";
+                    try {
+                        IPSNode navNode = (IPSNode) helper.findNavNode(assemblyItem);
+                        if (navNode != null) {
+                            path = getParentFolderPath(navNode.getGuid());
+                        }
+                        else {
+                            log.warn("Tried to use NavHelper to getParentFolderPath " +
+                                    "but no navon could be found.");
+                            path = null;
+                        }
+                    } catch (PSCmsException e) {
+                        log.error(errMesg, e);
+                        throw new RuntimeException(e);
+                    } catch (PSFilterException e) {
+                        log.error(errMesg,e);
+                        throw new RuntimeException(e);
+                    } 
+                    catch (RepositoryException e) {
+                        log.warn("Could not find folder using NavHelper: ", e);
+                        path = null;
+                    }
+                }
+                else {
+                    log.debug("Could not use NavHelper to find folderid because the" +
+                            " provided assembly item did not have one. (getNavHelper() == null)");
+                    path = null;
+                }
+            }
+        } 
+        else {
+            log.debug("Using AssemblyItem's folderid = " + id);
+            IPSGuid folderGuid = guidManager.makeGuid(new PSLocator(id, -1));
+            List<PSFolder> folders = contentWs.loadFolders(asList(folderGuid));
+            if (folders.size() < 1) {
+                path = null;
+            } 
+            else {
+                path = folders.get(0).getFolderPath();
+            }
+        }
+        return path;
+    }
 
    @Override
     public void init(IPSExtensionDef def, File codeRoot)
