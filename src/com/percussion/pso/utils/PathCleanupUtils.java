a633bf1220c1ef368d1521b58169e24acff8bc80
stephenbolton <stephen_bolton@percussion.com>
2012-05-15 13:31:39 -0400

    added extra options for copy parameter to manipulate adding prefix, suffix and extension manipulation

--- src/com/percussion/pso/utils/PathCleanupUtils.java
+++ src/com/percussion/pso/utils/PathCleanupUtils.java
@@ -1,23 +1,39 @@
 package com.percussion.pso.utils;
 
+import org.apache.commons.lang.StringUtils;
+
 public class PathCleanupUtils {
 
-	public static String cleanupPathPart(String path,boolean forceLower,boolean includesExtension) {
-		String ext="";
+	public static String cleanupPathPart(String path,boolean forceLower,boolean includesExtension, boolean stripExtension, String prefix, String suffix, String forceExtension) {
+		String ext=null;
 	    path = path.replaceAll("[&]", " and ");
 	    path = path.replaceAll("[^0-9a-zA-Z-_/ \\.\\\\]", "");
+	 
 	    int extIndex = path.lastIndexOf(".");
 	    if (forceLower) path=path.toLowerCase();
+		if (StringUtils.isNotEmpty(forceExtension)) {
+			ext = forceExtension;
+		}
 		if (extIndex > 0 && includesExtension) {
-			ext=path.substring(extIndex+1,path.length());
 		   path=path.substring(0,extIndex);
 		   path = path.replaceAll("[-_ \\.\\\\]+", "-");
-		   ext = ext.replaceAll("[-_ \\.\\\\]+", "-");
-			if (forceLower) ext=ext.toLowerCase();
-			path=path+"."+ext;
+		   if (!stripExtension && ext==null) {
+			   ext=path.substring(extIndex+1,path.length());
+			   ext = ext.replaceAll("[-_ \\.\\\\]+", "-");
+			   if (forceLower) ext=ext.toLowerCase();
+		   }
 		} else {
 			path = path.replaceAll("[-_ \\.\\\\]+", "-");
 		}
+		
+	
+		
+		path=prefix + path+ suffix;
+		
+	    if (ext!=null) {
+	    	path+="."+ext;
+	    }
+		
 	    return path;
 	}
 	
